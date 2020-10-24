6ef7e3fe613d675634b8b63ce9e292a0b5bdeddd
ervasevim <ervasevim@gmail.com>
2020-10-24 23:01:02 +0300

    tasks.blade UPDATE.

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -2,9 +2,13 @@
 <html>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <body>
+
+    <div class="ml-5 mt-4">
+        <h5 style="color: darkslategray;">Tüm Görevlerin Toplam {{count($weeks)}} Haftada Bitmesi Planlanmıştır.</h5>
+    </div>
     @foreach($weeks as $ind => $week)
         <div class=" ml-5 mt-5">
-            <h2>{{$ind+1}}. Hafta</h2>
+            <h2 style="color: midnightblue;">{{$ind+1}}. Hafta</h2>
         </div>
         <div class="p-5">
             <table class="table">
